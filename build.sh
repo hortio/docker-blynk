e9335f518fd004f215b81b9da0d64434dea6b214
Sergei Silnov <po@kumekay.com>
2019-08-13 22:37:51 +0200

    up to v0.41.10

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 VERSION_MAJOR_MINOR=0.41
-VERSION_PATCH=9
+VERSION_PATCH=10
 
 docker build . -t hortio/blynk:latest -t hortio/blynk:v${VERSION_MAJOR_MINOR}.${VERSION_PATCH} -t hortio/blynk:v${VERSION_MAJOR_MINOR}
 docker push hortio/blynk:latest
